f85ec7d052bbcaf4d3aa76dd6e1d2b3ff010e014
Ryan Peterson <ryanjpeterson93@gmail.com>
2020-03-05 07:19:17 -0700

    need to finish sortArray

---
+++ __tests__/app.test.js
@@ -0,0 +1,63 @@
+import {
+  originalArray,
+  doubleArray,
+  evensOnlyArray,
+  arraySum,
+  allNumbersGreaterThanZero,
+  someNumbersAreOdd,
+  evensOnlyAndDoubleArray,
+  findItem,
+  sortArray
+} from '../app'
+
+describe('originalArray Tests', ()=> {
+  test('originalArray should return it self', () => {
+    expect(originalArray([1,2,3,4])).toEqual([1,2,3,4])
+  })
+})
+
+describe('doubleArray Tests', ()=> {
+  test('doubleArray should double nums', () => {
+    expect(doubleArray([1,2,3,4])).toEqual([2,4,6,8])
+  }) 
+})
+
+describe('evensOnlyArray Tests', () =>{
+  test('evensOnlyArray should pull evens', ()=>{
+    expect(evensOnlyArray([1,2,3,4])).toEqual([2,4])
+  })
+})
+
+describe('arraySum tests', ()=> {
+  test('arraySum should sum all nums of array', ()=>{
+    expect(arraySum([1,2,3,4])).toEqual(10)
+  })
+})
+
+describe('allNumbersGreaterThanZero Test', ()=>{
+  test('allNumbersGreaterThanZero finds nums > 0', ()=>{
+    expect(allNumbersGreaterThanZero([1,2,3,4])).toBeTruthy()
+  })
+})
+
+describe('someNumbersAreOdd', ()=>{
+  test('someNumbersAreOdd returns true if odd is found', ()=>{
+    expect(someNumbersAreOdd([1,2,3,4])).toBeTruthy()
+  })
+})
+
+describe('evensOnlyAndDoubleArray', ()=>{
+  test('evensOnlyAndDoubleArray finds even numbers, then doubles those evens', ()=>{
+    expect(evensOnlyAndDoubleArray([1,2,3,4])).toEqual([4,8])
+  })
+})
+
+describe('findItem', ()=>{
+  test('findItem returns not found', ()=>{
+    expect(findItem([1,2,3,4], 'a')).toEqual('not found')
+  })
+  test('findItem returns not found', ()=>{
+    expect(findItem([1,2,3,4], 2)).toEqual(2)
+  })
+})
+
---
+++ app.js
@@ -0,0 +1,98 @@
+export const originalArray = (arr) => {
+  return arr
+}
+
+// EXPORT ALL AS FUNCTIONS AND KEEP THE NAMES THE SAME
+// ways to export
+// export const methodName = (param) => {
+// }
+// ----or---------
+// export methodName function(param){
+// }
+let numberArray = [1,2,3,4]
+// ARRAY ITERATOR METHODS
+
+// return array with double values in array
+// doubleArray(numberArray) // => [2,4,6,8];  hint use map 
+export const doubleArray = (numberArray) => {
+ return numberArray.map((num) => { 
+    return num * 2 
+  })
+}
+
+// return array with all even numbers
+// evensOnlyArray(numberArray) // => [2,4,6,8];  hint use filter 
+export const evensOnlyArray = (numberArray) => {
+  return numberArray.filter((num) => { 
+    return num % 2 ===0
+  })
+}
+// // return array with sum of numbers
+// arraySum(numberArray) // => 10;  hint use reduce
+export const arraySum = (numberArray) => {
+  return numberArray.reduce((a, b)=>{
+    return a + b
+  }, 0)
+}
+
+// // return true if every item greater than 0
+// allNumbersGreaterThanZero(numberArray) // => true; use every
+export const allNumbersGreaterThanZero = (numberArray) => {
+  return numberArray.every(function (e) {
+    return e > 0;
+  });
+}
+
+// // return true if some item greater than 0
+// someNumbersAreOdd(numberArray) // => true; use some
+export const someNumbersAreOdd = (numberArray) => {
+  return numberArray.some((a)=>{
+    return a % 2 !== 0
+  })
+ }
+
+// // return array double and even
+// evensOnlyAndDoubleArray(numberArray) // => [4,8]; // use map and filter
+export const evensOnlyAndDoubleArray = (numberArray) => {
+  var jamessux = evensOnlyArray(numberArray) 
+    return doubleArray(jamessux)
+}
+
+// // find an item method return 'not found' if not found
+// findItem(numberArray, 1) //=> 1
+export const findItem = (numberArray, searchItem) => {
+  if ((numberArray.find(a => a === searchItem))) {
+    return searchItem
+  } else { 
+    return 'not found'
+  }
+}
+
+// sortArray(numberArray) //=> [4,3,2,1] *desc*
+
+
+// // -----------------------
+// // BONUS STUFF DOWN THERE
+// // -----------------------
+// artists = [
+//   {name: 'a', likes:100},
+//   {name: 'c', likes:99},
+//   {name: 'c', likes:101}, 
+// ]
+// // ARRAY ITERATOR METHODS ON Objects with name age 
+// // doubles likes of artist
+// doubleLikes(artists)
+// // => [
+// //   {name: 'a', likes:200},
+// //   {name: 'c', likes:198},
+// //   {name: 'c', likes:202},  
+// // ]; 
+// moreThan100Likes(artists)// =>[ {name: 'c', likes:101} ]; // use filter
+// //  return an array of strings of artist name
+// justArtistMoreThan100Likes(artists) //=> ['c']; // use filter
+// // get total number of likes fo all artists
+// numberOfLikes(artists) //=> 300; // get all likes
+// // find an item method. Returns 'not found' if not found
+// findObjByName(artists, 'a') // => {name: 'a', likes:200}
+// // returns array of artist sorted by likes
+// sortArtistByName(artists)
